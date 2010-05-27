683d578fb5c8e4893495c185977d69083151ba6d
Adam Hughes <arhughes@gmail.com>
2010-05-27 09:13:56 -0400

    Add video after link, and remove video by clicking link again.

--- scripts/youtube_loader.js
+++ scripts/youtube_loader.js
@@ -27,44 +27,53 @@ settingsLoadedEvent.addHandler(function()
                 if (e.button == 0)
                 {
                     var link = this;
+                    // if there is a video after the link, remove it
+                    if (link.nextSibling != null && link.nextSibling.className == "youtubeloader")
+                    {
+                        link.parentNode.removeChild(link.nextSibling);
+                    }
+                    else
+                    {
+                        // no video after the link? add one in!
+                        var domain = String(link.href);
+                        if (domain.substr(0, 7) == "http://")
+                            domain = domain.substr(7);
+                        if (domain.indexOf('/') != -1)
+                            domain = domain.substring(0, domain.indexOf('/'));
 
-                    var domain = String(link.href);
-                    if (domain.substr(0, 7) == "http://")
-                        domain = domain.substr(7);
-                    if (domain.indexOf('/') != -1)
-                        domain = domain.substring(0, domain.indexOf('/'));
+                        var pos = link.href.indexOf("v=");
+                        if (pos == -1) return;
 
-                    var pos = link.href.indexOf("v=");
-                    if (pos == -1) return;
+                        var videoId = link.href.substr(pos + 2);
+                        videoId = videoId.split('&');
+                        videoId = videoId[0];
 
-                    var videoId = link.href.substr(pos + 2);
-                    videoId = videoId.split('&');
-                    videoId = videoId[0];
+                        var url = "http://" + domain + "/v/" + videoId + "&hl=en_US&fs=1";
 
-                    var url = "http://" + domain + "/v/" + videoId + "&hl=en_US&fs=1";
+                        var o = document.createElement("object");
+                        o.setAttribute("width", 640);
+                        o.setAttribute("height", 385);
+                        o.appendChild(YoutubeLoader.createParam("movie", url));
+                        o.appendChild(YoutubeLoader.createParam("allowFullScreen", "true"));
+                        o.appendChild(YoutubeLoader.createParam("allowscriptaccess", "always"));
 
-                    var o = document.createElement("object");
-                    o.setAttribute("width", 640);
-                    o.setAttribute("height", 385);
-                    o.appendChild(YoutubeLoader.createParam("movie", url));
-                    o.appendChild(YoutubeLoader.createParam("allowFullScreen", "true"));
-                    o.appendChild(YoutubeLoader.createParam("allowscriptaccess", "always"));
+                        var embed = document.createElement("embed");
+                        embed.setAttribute("src", url);
+                        embed.setAttribute("type", "application/x-shockwave-flash");
+                        embed.setAttribute("allowscriptaccess", "always");
+                        embed.setAttribute("allowfullscreen", "true");
+                        embed.setAttribute("width", "640");
+                        embed.setAttribute("height", "385");
 
-                    var embed = document.createElement("embed");
-                    embed.setAttribute("src", url);
-                    embed.setAttribute("type", "application/x-shockwave-flash");
-                    embed.setAttribute("allowscriptaccess", "always");
-                    embed.setAttribute("allowfullscreen", "true");
-                    embed.setAttribute("width", "640");
-                    embed.setAttribute("height", "385");
+                        o.appendChild(embed);
 
-                    o.appendChild(embed);
+                        var div = document.createElement("div");
+                        div.className = "youtubeloader";
+                        div.appendChild(o);
 
-                    var div = document.createElement("div");
-                    div.className = "youtubeloader";
-                    div.appendChild(o);
-                    link.removeChild(link.firstChild);
-                    link.appendChild(div);
+                        // add the video right after the link
+                        link.parentNode.insertBefore(div, link.nextSibling);
+                    }
                     
                     e.preventDefault();
                 }
