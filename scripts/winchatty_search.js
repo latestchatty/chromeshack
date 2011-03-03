69599f806408bee6554f162e328dcb1bcf752cbc
Adam Hughes <arhughes@gmail.com>
2011-03-03 11:27:01 -0500

    disable winchatty search in the searchbox

--- scripts/winchatty_search.js
+++ scripts/winchatty_search.js
@@ -3,10 +3,13 @@ WinChattySearch =
     install: function()
     {
         // redirect searchbox to winchatty
+        // disabled at least until electorly gets things working again
+        /*
         var searchbox = document.getElementById("search_box");
         searchbox.action = "http://winchatty.com/search.php";
         var text = document.getElementById("searchbox");
         text.name = "terms";
+        */
 
         var liUser = getDescendentByTagAndClassName(document.getElementById('user'), 'li', 'user');
         if (liUser != null)
