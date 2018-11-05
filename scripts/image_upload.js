815c24b3deb7c4aff215a4baac7c5ba04008befc
WombatFromHell <WombatFromHell@users.noreply.github.com>
2018-11-04 20:38:25 -0700

    General bug fixes and behavior improvements for image uploader

--- scripts/image_upload.js
+++ scripts/image_upload.js
@@ -55,11 +55,10 @@ settingsLoadedEvent.addHandler(function()
                                 <div id="uploadDropArea">
                                     <input type="file" id="fileUploadInput" multiple accept="image/*">
 
-                                    <span class="uploadDropLabel">
+                                    <div class="uploadDropLabelArea">
                                         <a href="#" id="fileChooserLink">Choose some files</a>
-                                        or drop them here...
-                                    </span>
-
+                                        <span class="uploadDropLabel">or drop them here...</span>
+                                    </div>
                                     <div class="urlBox">
                                         <input type="text" id="urlUploadInput"
                                             spellcheck="false"
@@ -119,12 +118,19 @@ settingsLoadedEvent.addHandler(function()
 
                 // toggle entry fields based on hoster
                 $("#uploadChatty").click(function() {
+                    // chattypics can take multiple files at once
+                    $("#fileChooserLink").text("Choose some files");
+                    $(".uploadDropLabel").text("or drop some here...");
+
                     $("#urlUploadInput").toggleClass('hidden', $("#uploadChatty").is(":checked"));
                     $("#fileUploadInput").attr("accept", "image/*");
                     $("#fileUploadInput").attr("multiple");
                     ImageUpload.clearFileData(true);
                 });
                 $("#uploadGfycat, #uploadImgur").click(function() {
+                    // gfycat and imgur only allow one file at a time
+                    $("#fileChooserLink").text("Choose a file");
+                    $(".uploadDropLabel").text("or drop one here...");
                     // gfycat allows video input
                     if ($(this).is("#uploadGfycat"))
                         $("#fileUploadInput").attr("accept", "image/*,video/*");
@@ -212,7 +218,9 @@ settingsLoadedEvent.addHandler(function()
                     $(".contextLine").removeClass("hidden");
                     $("#uploadDropArea").toggleClass("dragOver", ImageUpload.formFiles.length > 0);
                     // styling to indicate to the user that the files will be uploaded
-                    $("#uploadStatusLabel").toggleClass("muted", !ImageUpload.formFileUrl.length > 7);
+                    if (!ImageUpload.formFileUrl.length > 7)
+                        $("#uploadStatusLabel").removeClass("muted");
+
                     return true;
                 }
                 return false;
@@ -223,22 +231,31 @@ settingsLoadedEvent.addHandler(function()
                 if (ImageUpload.isValidUrl(string)) {
                     ImageUpload.formFileUrl = string;
                     $(".contextLine").removeClass("hidden");
+                    $("#urlUploadInput").removeClass("valid").addClass("valid");
+                    $("#uploadDropArea").removeClass("dragOver").addClass("dragOver");
                     // styling to indicate to the user that the url takes priority over files
                     $("#uploadStatusLabel").removeClass("muted").addClass("muted");
                     return true;
-                } else if (ImageUpload.formFiles == null) {
-                    ImageUpload.clearFileData();
+                } else if (ImageUpload.formFiles.length == 0) {
+                    // not a valid string yet we don't have files so wipe our saved url state
+                    ImageUpload.formFileUrl = "";
+                    $("#uploadDropArea").removeClass("dragOver");
+                    $("#uploadStatusLabel").removeClass("muted");
+                    $("#urlUploadInput").removeClass("valid");
+                    $(".contextLine").removeClass("hidden").addClass("hidden");
                     return true;
+                } else {
+                    $("#urlUploadInput").removeClass("valid");
+                    $("#uploadStatusLabel").removeClass("muted");
                 }
-
-                // remove file status styling because we have no url content
-                $("#uploadStatusLabel").toggleClass("muted", ImageUpload.isValidUrl(string));
                 return false;
             },
 
             clearFileData: function(soft) {
-                var _isUrl = ImageUpload.formFileUrl.length > 0;
-                var _isFiles = ImageUpload.formFiles != null;
+                // contextually reset our uploader form inputs
+                var _isUrl = ImageUpload.isValidUrl(ImageUpload.formFileUrl);
+                var _isFiles = ImageUpload.formFiles.length > 0;
+                var _isUrlInput = $("#urlUploadInput").val().length > 0;
 
                 // override for checking chattypics filter
                 if (soft) {
@@ -250,24 +267,30 @@ settingsLoadedEvent.addHandler(function()
                 }
 
                 if (_isFiles && _isUrl) {
-                    ImageUpload.formFiles = null;
+                    // if we have a valid url, wipe our files instead
+                    ImageUpload.formFiles = [];
                     ImageUpload.updateStatusLabel();
-                } else if (_isUrl) {
+                    $("#uploadStatusLabel").removeClass("muted");
+                } else if (_isUrlInput) {
+                    // wipe any content in the input box
                     ImageUpload.formFileUrl = "";
                     $("#urlUploadInput").val("");
-                    $(".contextLine").addClass("hidden");
+                    $("#uploadDropArea").removeClass("dragOver");
+                    $(".contextLine").removeClass("hidden").addClass("hidden");
                 } else if (_isFiles) {
-                    ImageUpload.formFiles = null;
+                    ImageUpload.formFileUrl = "";
+                    ImageUpload.formFiles = [];
                     ImageUpload.updateStatusLabel();
-                    $(".contextLine").addClass("hidden");
+                    $("#uploadStatusLabel").removeClass("muted");
                 }
 
+                ImageUpload.removeUploadMessage();
                 if (ImageUpload.formFileUrl.length == 0 &&
-                    ImageUpload.formFiles == null) {
-                        $("#uploadDropArea").removeClass("dragOver");
-                        $("#uploadStatusLabel").removeClass("muted").addClass("muted");
-                        ImageUpload.removeUploadMessage();
-                    }
+                    ImageUpload.formFiles.length == 0) {
+                    $("#uploadStatusLabel").removeClass("muted");
+                    $("#uploadDropArea").removeClass("dragOver");
+                    $(".contextLine").removeClass("hidden").addClass("hidden");
+                }
 
                 return false;
             },
@@ -289,7 +312,7 @@ settingsLoadedEvent.addHandler(function()
             isValidUrl: function(string) {
                 var ip_pattern = /^(?:http:\/\/|https:\/\/)*?(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/[\w\d\-\_\&\#\$\^\?\.\=\,\/\\]+\.(gif|jpg|jpeg|png)$/i;
                 var url_pattern = /^(?:http:\/\/|https:\/\/)*?([\d\w\-\.]+){1,}\/[\w\d\-\&\#\$\^\?\.\=\,\/\\]+\.(gif|jpg|jpeg|png)$/i;
-                var youtube_pattern = /^(?:http:\/\/|https:\/\/)(?:.*\.|)youtube\.[A-Za-z]{2,}\/watch\?v=[\w\d\-]+$/i;
+                var youtube_pattern = /^(?:http:\/\/|https:\/\/)(?:|youtu.be\/[\w\d\\-]+(\?t=\d+|)|(?:.*\.|)youtube\.[A-Za-z]{2,}\/watch\?v=[\w\d\-]+)$/i;
                 if (string.length > 7 && string.length < 2048) {
                     if(ip_pattern.test(string) ||
                         url_pattern.test(string) ||
@@ -332,7 +355,8 @@ settingsLoadedEvent.addHandler(function()
                     }
                     ImageUpload.doChattyPicsUpload(fd);
                 } else if (isImgur) {
-                    fd.append("file", filesList[0]);
+                    fd.append("type", "file");
+                    fd.append("image", filesList[0]);
                     ImageUpload.doImgurUpload(fd);
                 } else if (isGfycat) {
                     ImageUpload.doGfycatUpload({ "file": filesList[0] });
@@ -350,7 +374,7 @@ settingsLoadedEvent.addHandler(function()
                     cache: false,
                     contentType: false,
                     processData: false,
-                    enctype : 'multipart/form-data',
+                    enctype: 'multipart/form-data',
                     dataType: 'json',
                     beforeSend : ImageUpload.setImgurHeader,
                     data : formdata
@@ -384,7 +408,10 @@ settingsLoadedEvent.addHandler(function()
             handleUploadSuccess: function(respdata) {
                 var link = respdata.data.link;
                 $("#frm_body").insertAtCaret(link + "\n");
-                ImageUpload.addUploadMessage("green", "Success!");
+                ImageUpload.addUploadMessage("green", null, "Success!");
+                setTimeout(function() {
+                    ImageUpload.removeUploadMessage();
+                }, 3000);
             },
 
             handleChattyUploadSuccess : function(data) {
@@ -393,7 +420,10 @@ settingsLoadedEvent.addHandler(function()
                 var link = link11[0];
                 var url = $(link).val();
                 $("#frm_body").insertAtCaret(url + "\n");
-                ImageUpload.addUploadMessage("green", "Success!");
+                ImageUpload.addUploadMessage("green", null, "Success!");
+                setTimeout(function() {
+                    ImageUpload.removeUploadMessage();
+                }, 3000);
             },
 
             handleUploadFailure: function(respdata) {
