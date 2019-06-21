f8c34fd48389bf61487a395c28a0b5f7fa7508bf
Brian Luft <brian@electroly.com>
2019-06-21 02:01:03 +0000

    Two-pane mode - fix lol tags appearing in usernames
    Fixes #132

--- scripts/thread_pane.js
+++ scripts/thread_pane.js
@@ -155,7 +155,7 @@ let refreshThreadPane;
     }
 
     function parseRootAuthor($opDiv) {
-        const $rootAuthorAnchor = $opDiv.find('div.postmeta span.author span.user');
+        const $rootAuthorAnchor = $opDiv.find('div.postmeta span.author span.user a');
         if ($rootAuthorAnchor.length !== 1) {
             throw new Error(`Could not find the author username.`);
         }
