c0e2f8b50d2a6fc8bc41a92eeaf1431717f2677b
WombatFromHell <WombatFromHell@users.noreply.github.com>
2019-07-30 17:57:01 -0600

    Updated Shame Switchers for the nuLOL Chatty

--- scripts/switchers.js
+++ scripts/switchers.js
@@ -4,68 +4,64 @@
 
 let Switchers = {
     offenders: [
-        { original: "thaperfectdrug", new_name: "Dave-A", original_id: 2650 },
-        { original: "MagicWishMonkey", new_name: "MaximDiscord", original_id: 160547, alt_id: 174688 },
-        { original: "timaste", new_name: "timmytaste", original_id: 172749, alt_id: 10187646 },
-        { original: "The Grolar Bear", new_name: "The Gorilla Bear", original_id: 209153, alt_id: 10187648 },
-        { original: "jingletard", new_name: "Jingletardigrade", original_id: 194581, alt_id: 10196015 },
-        { original: "ArB", new_name: "jingleArB", original_id: 159879, alt_id: 10195980 },
-        { original: "Rigor Morts", new_name: "dewhickey", original_id: 173971, alt_id: 10196460 }
+        { old: "thaperfectdrug", new: "Dave-A" },
+        { old: "MagicWishMonkey", new: "MaximDiscord" },
+        { old: "timaste", new: "timmytaste" },
+        { old: "The Grolar Bear", new: "The Gorilla Bear" },
+        { old: "jingletard", new: "Jingletardigrade" },
+        { old: "ArB", new: "jingleArB" },
+        { old: "Rigor Morts", new: "dewhickey" }
     ],
 
-    loadSwitchers(item) {
-        for (let iOffender = 0; iOffender < Switchers.offenders.length; iOffender++) {
-            let offender = Switchers.offenders[iOffender];
-            let offenderPosts = getDescendentsByTagAndAnyClassName(
-                item,
-                "div",
-                "olauthor_" + offender.original_id
-            );
-            Switchers.rewritePosts(offenderPosts, offender);
-            offenderPosts = getDescendentsByTagAndAnyClassName(item, "div", "fpauthor_" + offender.original_id);
-            Switchers.rewritePosts(offenderPosts, offender);
-            // include the nuId if we have one
-            if (offender.alt_id != null) {
-                let offenderPosts = getDescendentsByTagAndAnyClassName(
-                    item,
-                    "div",
-                    "olauthor_" + offender.alt_id
-                );
-                Switchers.rewritePosts(offenderPosts, offender);
-                offenderPosts = getDescendentsByTagAndAnyClassName(item, "div", "fpauthor_" + offender.alt_id);
-                Switchers.rewritePosts(offenderPosts, offender);
+    resolved: [],
+
+    cacheSwitchers() {
+        // resolve and cache all offenders on the page once on load
+        let users = HighlightUsers.resolveUsers();
+        for (let user of users || []) {
+            for (let offender of Switchers.offenders) {
+                let matchedOld = offender.old.toLowerCase() === user.name.toLowerCase();
+                let matchedNew = offender.new.toLowerCase() === user.name.toLowerCase();
+                if (matchedOld || matchedNew)
+                    Switchers.resolved = [
+                        ...Switchers.resolved,
+                        {
+                            id: user.id,
+                            name: user.name,
+                            matched: matchedNew ? offender.old : offender.new
+                        }
+                    ];
             }
         }
     },
 
-    rewritePosts(offenderPosts, offender) {
-        if (offenderPosts) {
-            for (let iPost = 0; iPost < offenderPosts.length; iPost++) {
-                let post = offenderPosts[iPost];
+    loadSwitchers(item) {
+        for (let offender of Switchers.resolved || []) {
+            let offenderOLs = [...item.querySelectorAll(`div.olauthor_${offender.id}`)];
+            let offenderFPs = [...item.querySelectorAll(`div.fpauthor_${offender.id}`)];
+            let offenderPosts = [...offenderOLs, ...offenderFPs];
+            for (let post of offenderPosts)
+                Switchers.rewritePost(post, offender.name, offender.matched);
+        }
+    },
 
-                let newName = `${offender.original} - (${offender.new_name})`;
-                let span = getDescendentByTagAndClassName(post, "span", "oneline_user");
-                if (span) {
-                    //For single line comments.
-                    span.textContent = newName;
-                } //For fully shown comments.
-                else {
-                    span = getDescendentByTagAndClassName(post, "span", "user");
-                    if (span) {
-                        span.firstChild.textContent = newName;
-                    }
-                }
-
-                //Switchers don't deserve lightning
-                let lightningbolt = getDescendentByTagAndClassName(post, "a", "lightningbolt");
-                if (lightningbolt) {
-                    $(lightningbolt).hide();
-                }
-            }
+    rewritePost(post, name, oldName) {
+        let newName = `${name} - (${oldName})`;
+        let span = post.querySelector("span.oneline_user");
+        let alt_span = post.querySelector("span.user");
+        if (span) span.textContent = newName;
+        else if (alt_span) span.firstChild.textContent = newName;
+        // Switchers don't deserve flair icons
+        let user_icons = post.querySelectorAll("img.chatty-user-icons");
+        for (let icon of user_icons || []) {
+            icon.setAttribute("style", "display: none !important;");
         }
     }
 };
 
 addDeferredHandler(enabledContains("switchers"), res => {
-    if (res) processPostEvent.addHandler(Switchers.loadSwitchers);
+    if (res) {
+        Switchers.cacheSwitchers();
+        processPostEvent.addHandler(Switchers.loadSwitchers);
+    }
 });
