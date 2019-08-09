9d56df2f402dad54ecae2abb2bc468595de16d40
WombatFromHell <WombatFromHell@users.noreply.github.com>
2019-08-09 12:32:18 -0600

    Fixed regressions in HighlightPendingPosts

--- scripts/highlight_pending_new_posts.js
+++ scripts/highlight_pending_new_posts.js
@@ -5,68 +5,69 @@ let HighlightPendingPosts = {
 
     pendings: [],
 
-    processEvents(events) {
-        for (let evt of events || []) {
-            if (evt.eventType === "newPost") {
-                let postId = parseInt(evt.eventData.postId);
-                // Received an event for a post we already have.
-                if (document.getElementById(`item_${postId}`) !== null) continue;
-                let threadId = parseInt(evt.eventData.post.threadId);
-                let a = document.querySelector(`li#item_${threadId} div.fullpost div.refresh a`);
-                if (a !== null) a.classList.add("refresh_pending");
-            }
+    marked: [],
+
+    updatePendings(refreshElem) {
+        for (let pending of HighlightPendingPosts.pendings || []) {
+            // Received an event for a post we already have.
+            if (document.getElementById(`item_${pending.postId}`)) continue;
+            let a = document.querySelector(`li#item_${pending.threadId} div.fullpost div.refresh a`);
+            if (a) a.classList.add("refresh_pending");
         }
-        HighlightPendingPosts.getNonCollapsedPendings();
-        HighlightPendingPosts.showOrHideJumpToNewPostButton();
+        HighlightPendingPosts.updateJumpToNewPostButton(refreshElem);
         // if the Thread Pane script is loaded, then refresh the thread pane
-        if (typeof refreshThreadPane !== "undefined") refreshThreadPane();
+        if (refreshThreadPane) refreshThreadPane();
     },
 
     fetchPendings() {
         fetchSafe(`https://winchatty.com/v2/waitForEvent?lastEventId=${HighlightPendingPosts.lastEventId}`)
-            .then((json) => {
-                // sanitized in common.js!
-                if (json.events) {
-                    HighlightPendingPosts.lastEventId = parseInt(json.lastEventId);
-                    HighlightPendingPosts.processEvents(json.events);
+        .then((json) => {
+            // sanitized in common.js!
+            if (json.events) {
+                HighlightPendingPosts.lastEventId = parseInt(json.lastEventId);
+                let newPosts = [...json.events.filter(x => x.eventType === "newPost")];
+                let newPendingEvents = [];
+                for (let post of newPosts) {
+                    newPendingEvents.push({
+                        postId: parseInt(post.eventData.postId),
+                        threadId: parseInt(post.eventData.post.threadId)
+                    });
                 }
-                // Short delay in between loop iterations.
-                setTimeout(HighlightPendingPosts.fetchPendings, 5000);
-            });
+                HighlightPendingPosts.pendings = newPendingEvents;
+                HighlightPendingPosts.updatePendings();
+            }
+            // Short delay in between loop iterations.
+            setTimeout(HighlightPendingPosts.fetchPendings, 5000);
+        });
     },
 
-    isCollapsed(aRefresh) {
-        let divRefresh = aRefresh.parentNode;
-        if (!divRefresh) return false;
-        let divFullpost = divRefresh.parentNode;
-        if (!divFullpost) return false;
-        let li = divFullpost.parentNode;
-        if (!li) return false;
-        let ul = li.parentNode;
-        if (!ul) return false;
-        let root = ul.parentNode;
-        return root && root.tagName == "DIV" &&
-            root.className.split(" ").indexOf("collapsed") !== -1;
+    excludeRefreshed(refreshElem) {
+        if (!refreshElem) return;
+        let closestId = refreshElem && refreshElem.closest("li[id^='item_']").id.substr(5);
+        let mutated = [...HighlightPendingPosts.pendings
+            .filter(x => x.postId !== closestId || x.threadId !== closestId)];
+        HighlightPendingPosts.pendings = mutated;
     },
 
-    getNonCollapsedPendings() {
-        let pendings = document.getElementsByClassName("refresh_pending");
+    isCollapsed(pending) {
+        return !!pending.closest("div.root.collapsed");
+    },
+
+    isPending(elem) {
+        return elem && refreshElem.closest("div.refresh a") === elem;
+    },
+
+    getNonCollapsedPendings(refreshElem) {
+        HighlightPendingPosts.excludeRefreshed(refreshElem);
+        let pendings = [...document.querySelectorAll("a.refresh_pending")];
         let filtered = [];
-        for (let pending of pendings || []) {
-            if (!HighlightPendingPosts.isCollapsed(pending))
+        for (let pending of pendings) {
+            // include only non-refreshed/non-collapsed pendings
+            if (!HighlightPendingPosts.isPending(refreshElem) &&
+                !HighlightPendingPosts.isCollapsed(pending))
                 filtered.push(pending);
         }
-        HighlightPendingPosts.pendings = filtered;
-    },
-
-    jumpToNewPost(e) {
-        e.preventDefault();
-        let pendingLen = HighlightPendingPosts.pendings.length;
-        let newIndex = (HighlightPendingPosts.lastIndex + 1 + pendingLen) % pendingLen;
-        let divPostItem = HighlightPendingPosts.pendings[newIndex] &&
-            HighlightPendingPosts.pendings[newIndex].parentNode.parentNode.parentNode;
-        scrollToElement(divPostItem);
-        HighlightPendingPosts.lastIndex = newIndex;
+        HighlightPendingPosts.marked = filtered;
     },
 
     installJumpToNewPostButton() {
@@ -76,20 +77,30 @@ let HighlightPendingPosts = {
         starContainer.setAttribute("id", "post_highlighter_container");
         starContainer.classList.add("hidden");
         star.setAttribute("id", "jump_to_new_post");
-        star.addEventListener("click", HighlightPendingPosts.jumpToNewPost);
+        star.addEventListener("click", (e) => {
+            e.preventDefault();
+            // simple incrementing carousel
+            let pendingLen = HighlightPendingPosts.marked.length;
+            let newIndex = (HighlightPendingPosts.lastIndex + 1 + pendingLen) % pendingLen;
+            let divPostItem = HighlightPendingPosts.marked[newIndex] &&
+                HighlightPendingPosts.marked[newIndex].closest("li[id^='item_']");
+            scrollToElement(divPostItem);
+            HighlightPendingPosts.lastIndex = newIndex;
+        });
         starContainer.appendChild(star);
         position.appendChild(starContainer);
     },
 
-    showOrHideJumpToNewPostButton() {
+    updateJumpToNewPostButton(refreshElem) {
         let button = document.getElementById("post_highlighter_container");
         let indicator = "★ ";
         let titleHasIndicator = document.title.startsWith(indicator);
-        if (HighlightPendingPosts.pendings.length > 0) {
+        let pendingPostBtn = document.getElementById("jump_to_new_post");
+        HighlightPendingPosts.getNonCollapsedPendings(refreshElem);
+        if (HighlightPendingPosts.marked.length > 0) {
             if (button) button.classList.remove("hidden");
             if (!titleHasIndicator) document.title = indicator + document.title;
-            $(document.getElementById("jump_to_new_post"))
-                .html(indicator + HighlightPendingPosts.pendings.length.toString());
+            pendingPostBtn.innerText = indicator + HighlightPendingPosts.marked.length.toString();
         } else {
             if (button) button.classList.add("hidden");
             if (titleHasIndicator) document.title = document.title.substring(indicator.length);
@@ -103,16 +114,17 @@ let HighlightPendingPosts = {
         let aSelectedPages = document.getElementsByClassName("selected_page");
         if (aSelectedPages.length === 0 || aSelectedPages[0].innerHTML !== "1") return;
         HighlightPendingPosts.installJumpToNewPostButton();
-        // Recalculate the "jump to new post" button's visibility when the user refreshes a thread.
-        document.getElementById("dom_iframe").addEventListener("load", () => {
-            // This is fired BEFORE the onload inside the Ajax response, so we need to wait until
-            // the inner onload has run.
-            setTimeout(HighlightPendingPosts.showOrHideJumpToNewPostButton, 0);
-        });
-        // Trying to get a notification when the user collapses a post.  This is easy...
-        document.addEventListener("click", () => {
-            // Same trick as above; let's wait until other events have executed.
-            setTimeout(HighlightPendingPosts.showOrHideJumpToNewPostButton, 0);
+        // Recalculate the "jump to new post" button's visibility when the user refreshes/toggles a thread
+        document.addEventListener("click", (e) => {
+            if (e.target.matches("div.refresh a") ||
+                e.target.matches("a.closepost") || e.target.matches("a.showpost")) {
+                (refreshElem => {
+                    document.getElementById("dom_iframe").addEventListener("load", () => {
+                        HighlightPendingPosts.updatePendings();
+                        HighlightPendingPosts.updateJumpToNewPostButton(refreshElem);
+                    });
+                })(e.target);
+            }
         });
         // We need to get an initial event ID to start with.
         fetchSafe("https://winchatty.com/v2/getNewestEventId").then((json) => {
