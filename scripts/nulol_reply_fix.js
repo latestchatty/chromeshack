fa5711de3730f9fdc8b615df6c99e5df52034c52
WombatFromHell <WombatFromHell@users.noreply.github.com>
2019-08-26 19:10:39 -0600

    More refactoring for nuLOL reply fix

--- scripts/nulol_reply_fix.js
+++ scripts/nulol_reply_fix.js
@@ -5,6 +5,7 @@
 processReplyEvent.addHandler((item, root) => {
     let rootPostRefreshBtn = root.querySelector(".fullpost.op .refresh > a");
     let refreshedOL = item.querySelector("span.oneline_body");
+    let refreshedBtn = item.querySelector(".refresh > a");
     // try to fix busted nuLOL tag data loading when replying
     if (rootPostRefreshBtn) {
         delayPromise(250)
@@ -12,6 +13,7 @@ processReplyEvent.addHandler((item, root) => {
             .then(Promise.resolve(rootPostRefreshBtn.click()))
             // ... then re-open the refreshed post
             .then(Promise.resolve(refreshedOL.click()))
-            .then(console.log("nuLOL reply workaround ran"));
+            .then(delayPromise(0).then(refreshedBtn.click()))
+            .catch(e => console.log("Something went wrong:", e));
     }
 });
