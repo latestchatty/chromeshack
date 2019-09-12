1d42278e4af19dc50c2adc5bd2f3b3eeaf2e1c53
WombatFromHell <WombatFromHell@users.noreply.github.com>
2019-09-12 11:19:21 -0600

    Minor optimization for CUF thread mode detection

--- scripts/custom_user_filters.js
+++ scripts/custom_user_filters.js
@@ -1,6 +1,8 @@
 let CustomUserFilters = {
     parsedUsers: [],
 
+    rootPostCount: 0,
+
     resolveUser(username) {
         // cache parsed page users locally (using HighlightUsers' resolver)
         if (CustomUserFilters.parsedUsers.length === 0)
@@ -11,7 +13,6 @@ let CustomUserFilters = {
     removeOLsFromUserId(id) {
         getSetting("cuf_hide_fullposts").then(hideFPs => {
             let postElems;
-            let rootChildren = [...document.querySelectorAll(".threads > .root")];
             if (hideFPs) postElems = [...document.querySelectorAll(`div.olauthor_${id}, div.fpauthor_${id}`)];
             else postElems = [...document.querySelectorAll(`div.olauthor_${id}`)];
             for (let post of postElems || []) {
@@ -20,7 +21,7 @@ let CustomUserFilters = {
                 let root = fp && fp.closest(".root");
                 if (ol && ol.parentNode.matches("li"))
                     ol.parentNode.removeChild(post);
-                else if (fp && root && rootChildren && rootChildren.length > 1)
+                else if (fp && root && CustomUserFilters.rootPostCount > 2)
                     // only remove root if we're in thread mode
                     root.parentNode.removeChild(root);
             }
@@ -30,6 +31,7 @@ let CustomUserFilters = {
     applyFilter() {
         getSetting("user_filters").then(filteredUsers => {
             if (!filteredUsers || filteredUsers.length === 0) return;
+            CustomUserFilters.rootPostCount = document.querySelector(".threads").childElementCount;
             for (let filteredUser of filteredUsers) {
                 let userMatches = CustomUserFilters.resolveUser(filteredUser);
                 for (let userMatch of userMatches)
