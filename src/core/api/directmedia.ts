3c172ebaa193e4cede88ca7b3d5971a3e68c0c1d
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-09-01 14:32:07 -0600

    Detect more valid directmedia links

--- src/core/api/directmedia.ts
+++ src/core/api/directmedia.ts
@@ -2,7 +2,7 @@ import { isImage, isVideo } from "../common";
 import type { ParsedResponse } from "./";
 
 const parseLink = (href: string) => {
-    const mediaMatch = /https?:\/\/.+?\/([\w\-._@#$%^&!()[\]{}']+\.(?:png|jpe?g|web[mp]|gifv?|mp4))(?:[&?]?.+|$)/i.exec(
+    const mediaMatch = /https?:\/\/.+?\/([\w\-._@#$%^&!()[\]{}+']+\.(?:png|jpe?g|web[mp]|gifv?|mp4))(?:[&?]?.+|$)/i.exec(
         href,
     );
     const type = mediaMatch && isImage(mediaMatch[1]) ? "image" : mediaMatch && isVideo(mediaMatch[1]) ? "video" : null;
