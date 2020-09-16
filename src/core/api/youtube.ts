e83b4753525f7c55d0d79bfb7fce50dbb8f931fd
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-09-16 09:56:40 -0600

    Fix some Youtube regex bugs

--- src/core/api/youtube.ts
+++ src/core/api/youtube.ts
@@ -1,31 +1,43 @@
 import type { ParsedResponse } from "./";
 
+const decodeOffset = (text: string) => {
+    const timeMatch = /(?:(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s))|(\d+)/i.exec(text);
+    const hour = timeMatch && timeMatch[1];
+    const min = timeMatch && timeMatch[2];
+    const sec = (timeMatch && timeMatch[3]) || timeMatch[4];
+    let offset = 0;
+    if (hour) offset += parseInt(hour) * 60;
+    if (min) offset += parseInt(min) * 60;
+    if (sec) offset += parseInt(sec);
+    return offset ? `&start=${offset}` : "";
+};
+
 const parseLink = (href: string) => {
-    // youtube videos and/or playlists (vid id: $1 || $3, playlist id: $2, offset: $4)
-    const isYoutube = /https?:\/\/(?:.+\.)?(?:youtu.be\/([\w-]+)(?:\?t=(\d+))?|youtube\..+?\/(?:(?:.+v=|embed\/)([\w-]+))(?:&t=(\d+)?|&list=([\w-]+))?(?:.+&t=(\d+))?)/i.exec(
+    // youtube videos and/or playlists (vid id: $2, playlist id: $3, offset: $1 || $4)
+    const isYoutube = /https?:\/\/(?:.+\.)?youtube\..+?\/(?:(?:embed\/|watch\?.*?(?:time_continue=(\w+))?[&?]?v=|)([\w-]+))(?:.*?&list=([\w-]+))?(?:(?:.*?[&?#]t=|.*?[&?]start=)(\w+))?/i.exec(
         href,
     );
-    // youtu.be videos
+    // youtu.be videos w/wo offset (video id: $1, offset: $2)
     const isYoutubeShort = /https?:\/\/youtu\.be\/([\w-]+)(?:[&?]?t=(\d+))?/i.exec(href);
 
-    const video = isYoutube ? isYoutube[1] || isYoutube[3] : isYoutubeShort ? isYoutubeShort[1] : "";
-    const playlist = isYoutube ? isYoutube[2] : "";
-    const offset = isYoutube ? isYoutube[4] || isYoutube[6] : isYoutubeShort ? isYoutubeShort[2] : "";
-    const startAt = offset ? `&start=${offset}` : "";
+    const video = isYoutube ? isYoutube[2] : isYoutubeShort ? isYoutubeShort[1] : "";
+    const playlist = isYoutube && isYoutube[3] ? `&list=${isYoutube[3]}` : "";
+    const offset = isYoutube ? isYoutube[1] || isYoutube[4] : isYoutubeShort ? isYoutubeShort[2] : "";
+    const startAt = offset ? decodeOffset(offset) : "";
 
     const h = { type: "iframe" };
-    if (video && !playlist)
+    if (video)
         return {
             ...h,
-            src: `https://www.youtube.com/embed/${video}?autoplay=1${startAt}`,
-        } as ParsedResponse;
-    else if (video && playlist)
-        return {
-            ...h,
-            src: `https://www.youtube.com/embed/${video}?autoplay=1&list=${playlist}${startAt}`,
+            href,
+            src: `https://www.youtube.com/embed/${video}?autoplay=1${playlist}${startAt}`,
         } as ParsedResponse;
     else if (!video && playlist)
-        return { ...h, src: `https://www.youtube.com/embed/videoseries?autoplay=1&list=${playlist}` } as ParsedResponse;
+        return {
+            ...h,
+            href,
+            src: `https://www.youtube.com/embed/videoseries?autoplay=1${playlist}`,
+        } as ParsedResponse;
     else return null;
 };
 
