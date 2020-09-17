7b05554839d5861993f2f45ca6b25e4d625edd80
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-09-16 18:38:06 -0600

    Fixed many Twitch regex bugs

--- src/core/api/twitch.ts
+++ src/core/api/twitch.ts
@@ -1,43 +1,38 @@
 import type { ParsedResponse } from "./";
 
 const parseLink = (href: string) => {
-    // twitch channels, videos, and clips (with time offset)
-    const isTwitch = /https?:\/\/(?:clips\.twitch\.tv\/(\w+)|(?:.*\.)?twitch\.tv\/(?:.*?\/clip\/(\w+)|(?:videos\/([\w-]+)(?:.*?t=(\w+))?|collections\/([\w-]+))|([\w-]+)))/i.exec(
+    // $1 = short form lips, $2 = long form clips, $2 or $3 = long form VODs, $4 = channels, $5 = collections, $6 = time offset
+    const isTwitch = /https?:\/\/(?:clips\.twitch\.tv\/(\w+)$|.*?\.twitch\.tv\/(?:(?:\w+\/clip\/|\w+\/v\/)(\w+)|videos\/(\w+)|(\w+).?$)(?:.*?\??collection=([\w-]+))?(?:.*?\??t=(\w+)$)?)/i.exec(
         href,
     );
+
     if (isTwitch) {
-        // twitch channels
-        const h = { type: "iframe" };
-        if (isTwitch[6])
-            return {
-                ...h,
-                src: `https://player.twitch.tv/?channel=${isTwitch[6]}&autoplay=true&muted=false&parent=www.shacknews.com`,
-            } as ParsedResponse;
-        // twitch collections
-        else if (isTwitch[5])
-            return {
-                ...h,
-                src: `https://player.twitch.tv/?collection=${isTwitch[5]}&autoplay=true&muted=false&parent=www.shacknews.com`,
-            } as ParsedResponse;
-        // twitch videos
-        else if (isTwitch[3])
-            return {
-                ...h,
-                src: `https://player.twitch.tv/?video=v${
-                    isTwitch[3]
-                }&autoplay=true&muted=false&parent=www.shacknews.com${isTwitch[4] ? `&t=${isTwitch[4]}` : ""}`,
-            } as ParsedResponse;
-        // twitch clips
-        else if (isTwitch[1] || isTwitch[2])
-            return {
-                ...h,
-                src: `https://clips.twitch.tv/embed?clip=${
-                    isTwitch[1] || isTwitch[2]
-                }&autoplay=true&muted=false&parent=www.shacknews.com`,
-            } as ParsedResponse;
-    } else {
-        return null;
+        const twitchClip = isTwitch[1] || isTwitch[2] || null;
+        const twitchVOD = isTwitch[2] || isTwitch[3] || null;
+        const twitchChannel = isTwitch[4] || null;
+        const twitchCollection = isTwitch[5] || null;
+        const twitchVODOffset = isTwitch[6] || null;
+
+        const basePlayerUrl = "https://player.twitch.tv/?";
+        const baseClipUrl = "https://clips.twitch.tv/embed?clip=";
+        const endUrl = "&parent=www.shacknews.com&autoplay=true&muted=false";
+
+        const channel = twitchChannel ? `channel=${twitchChannel}` : "";
+        const video = twitchVOD ? `video=v${twitchVOD}` : "";
+        const videoOffset = twitchVODOffset ? `&time=${twitchVODOffset}` : "";
+        const collection = twitchCollection ? `collection=${twitchCollection}` : "";
+        const clip = twitchClip || "";
+        const h = { type: "iframe", href };
+
+        if (video && !collection)
+            return { ...h, src: `${basePlayerUrl}${video}${endUrl}${videoOffset}` } as ParsedResponse;
+        else if (video && collection)
+            return { ...h, src: `${basePlayerUrl}${video}&${collection}${endUrl}${videoOffset}` } as ParsedResponse;
+        else if (collection) return { ...h, src: `${basePlayerUrl}${collection}${endUrl}` } as ParsedResponse;
+        else if (channel) return { ...h, src: `${basePlayerUrl}${channel}${endUrl}` } as ParsedResponse;
+        else if (clip) return { ...h, src: `${baseClipUrl}${clip}${endUrl}` } as ParsedResponse;
     }
+    return null;
 };
 
 export const isTwitch = (href: string) => parseLink(href);
