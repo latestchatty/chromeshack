4f2ca6c1e3ad67042f57db8e7236aa2917b7c682
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-09-17 11:24:12 -0600

    More refactoring of URL hook

--- src/core/useResolvedLinks/index.tsx
+++ src/core/useResolvedLinks/index.tsx
@@ -41,7 +41,7 @@ const loadComponent = (opts: URLProps) => {
     else return null;
 };
 
-const resolveResponse = async (opts: URLProps) => {
+const resolveComponent = async (opts: URLProps): Promise<JSX.Element> => {
     // takes a ParsedResponse (usually from Expando or resolveLink) and returns a media component
     const { response, options } = opts || {};
     const { href, src, component, args, cb } = (response as ParsedResponse) || {};
@@ -49,89 +49,61 @@ const resolveResponse = async (opts: URLProps) => {
     const resolved = cb && (await cb(...args));
     if (React.isValidElement(component) || React.isValidElement(resolved?.component))
         return component || resolved?.component;
-    // if the resolver callback returns an array of links just return them
-    else if (arrHas(resolved)) return resolved;
-    // otherwise we just return a component from the provided 'src' in the ParsedResponse
+    // if we have a list of responses (Imgur) then load them into media components
+    const aResolved =
+        arrHas(resolved) && resolved[0].src
+            ? (resolved as ParsedResponse[]).reduce((acc: JSX.Element[], r, i) => {
+                  const _component = r?.src && loadComponent({ response: r, options, key: i.toString() });
+                  if (React.isValidElement(_component)) acc.push(_component);
+                  return acc;
+              }, [])
+            : null;
+    if (arrHas(aResolved) && aResolved.length > 1) return <Carousel slides={aResolved} />;
+    // edge case: Imgur single-media album
+    else if (arrHas(aResolved)) return aResolved[0];
+    // otherwise we load a component from our resolver
     else
         return resolved?.src || src
             ? loadComponent({ response: resolved || response, options, key: src || href })
             : null;
 };
 const resolveLink = async (opts: URLProps) => {
-    // takes either a url or a ParsedResponse and resolves its async callback
-    const { link, response, options, key } = opts || {};
-    const detected = link && (await detectMediaLink(link));
-    const _response = (response as ParsedResponse) || detected;
-    const resolved = _response?.cb ? await resolveResponse({ response: _response, options }) : _response;
-    const component = React.isValidElement(resolved)
-        ? resolved
-        : resolved?.src
-        ? loadComponent({ response: resolved, options, key })
-        : null;
-    return component || null;
-};
-const resolveLinks = async (opts: URLProps) => {
-    // takes urls or ResolvedResponses and resolves them into a list of media components
-    const { links, responses, options } = opts || {};
-    // otherwise try to resolve into media components
-    const resolvedSources: JSX.Element[] = arrHas(links)
-        ? await links.reduce(async (acc: Promise<JSX.Element[]>, l, i) => {
-              // we assume our link is a direct media url
-              const resolved = await resolveLink({ link: l, options });
-              const _acc = await acc;
-              if (resolved) _acc.push(resolved);
-              return _acc;
-          }, Promise.resolve([]))
-        : null;
-    const resolvedResponses: JSX.Element[] = arrHas(responses)
-        ? await responses.reduce(async (acc: Promise<JSX.Element[]>, r, i) => {
-              const resolved = await resolveResponse({ response: r, options, key: i });
-              const _acc = await acc;
-              if (resolved) _acc.push(resolved);
-              return _acc;
-          }, Promise.resolve([]))
-        : null;
-    return resolvedSources || resolvedResponses;
+    const { link, options } = opts || {};
+    const detected = typeof link === "string" && (await detectMediaLink(link));
+    const resolved = detected && (await resolveComponent({ response: detected, options }));
+    // if we're provided a component return it
+    if (React.isValidElement(resolved) || React.isValidElement(detected?.component))
+        return detected?.component || resolved;
+    // otherwise load a media component from our response
+    const pResolved = resolved as ParsedResponse;
+    const rComponent = pResolved?.component && React.isValidElement(pResolved.component) ? pResolved.component : null;
+    const lComponent = pResolved?.src ? loadComponent({ response: pResolved, options }) : null;
+    return rComponent || lComponent;
 };
 const resolveAlbum = async (opts: URLProps) => {
-    const { links, responses, components, options } = opts || {};
-    // don't try to resolve an existing list of media components
-    const _components = arrHas(components) && React.isValidElement(components[0]) ? components : null;
-    if (arrHas(_components)) return <Carousel slides={_components} />;
-    // since we've got either a list of links or response objects let's resolve them
-    const slides = arrHas(links)
-        ? await resolveLinks({ links, options })
-        : arrHas(responses)
-        ? await resolveLinks({ responses, options })
-        : null;
-    // return a Carousel if we have enough slides
-    // edge case: return the first component in case of Imgur single-item albums
-    return arrHas(slides) && slides.length > 1 ? <Carousel slides={slides} /> : arrHas(slides) ? slides[0] : null;
+    const { links, options } = opts || {};
+    try {
+        if (arrHas(links) && typeof links[0] !== "string") throw Error("resolveAlbum only resolves a list of strings!");
+        // resolve urls into a list of media components
+        const resolved = [] as JSX.Element[];
+        for (const link of links || []) {
+            const _resolved = await resolveLink({ link, options });
+            if (React.isValidElement(_resolved)) resolved.push(_resolved);
+        }
+        // wrap media in a Carousel if necessary
+        if (arrHas(resolved) && resolved.length > 1) return <Carousel slides={resolved} />;
+        else if (arrHas(resolved)) return resolved[0];
+        else return null;
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 export const resolveChildren = async (opts: URLProps) => {
     const { links, response, options } = opts || {};
-    const rResolved = response && (await resolveResponse({ response, options }));
-    const lResolved = links && (await resolveLinks({ links, options }));
-    // return a media component(s) from our resolved results
-    const _component = React.isValidElement(rResolved) ? rResolved : React.isValidElement(lResolved) ? lResolved : null;
-    // otherwise attempt to resolve into a media component
-    const _album =
-        arrHas(lResolved) && React.isValidElement(lResolved[0])
-            ? lResolved
-            : arrHas(rResolved) && !React.isValidElement(rResolved)
-            ? rResolved
-            : arrHas(lResolved) && !React.isValidElement(lResolved)
-            ? lResolved
-            : null;
-    if (_album) {
-        const resolved = !React.isValidElement(_album[0])
-            ? await resolveAlbum({ responses: _album })
-            : (await resolveAlbum({ components: _album })) || null;
-        return React.isValidElement(resolved) ? resolved : null;
-    } else if (_component) {
-        return _component;
-    }
+    const cResolved = response && (await resolveComponent({ response, options }));
+    const lResolved = arrHas(links) && resolveAlbum({ links, options });
+    return cResolved || lResolved;
 };
 export const useResolvedLinks = (props: URLProps) => {
     // takes a url(s) or response(s) and exposes media component(s) and a load-state boolean
