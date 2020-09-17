82b37785e069f3f772d0e75d8a67c817229d9aaa
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-09-17 14:11:37 -0600

    Fix some regressions in Twitch matching

--- src/core/api/twitch.ts
+++ src/core/api/twitch.ts
@@ -1,38 +1,40 @@
 import type { ParsedResponse } from "./";
 
 const parseLink = (href: string) => {
-    // $1 = short form lips, $2 = long form clips, $2 or $3 = long form VODs, $4 = channels, $5 = collections, $6 = time offset
-    const isTwitch = /https?:\/\/(?:clips\.twitch\.tv\/(\w+)$|.*?\.twitch\.tv\/(?:(?:\w+\/clip\/|\w+\/v\/)(\w+)|videos\/(\w+)|(\w+).?$)(?:.*?\??collection=([\w-]+))?(?:.*?\??t=(\w+)$)?)/i.exec(
+    // $1 = VODs, $2 = channels, $3 = collections, $4 = time offset
+    const isTwitch = /https?:\/\/(?!clips\.)(?:.*?\.)?twitch\.tv\/(?:(?:\w+\/v\/|videos\/)(\w+)|(\w+).?$)(?:.*?\??collection=([\w-]+))?(?:.*?\??t=(\w+)$)?/i.exec(
         href,
     );
+    // $1 = short form clips, $2 = long form clips
+    const isTwitchClip = /https?:\/\/(?:clips\.twitch\.tv\/(\w+)|.*?\.twitch\.tv\/[\w-]+\/clip\/(\w+))/i.exec(href);
 
-    if (isTwitch) {
-        const twitchClip = isTwitch[1] || isTwitch[2] || null;
-        const twitchVOD = isTwitch[2] || isTwitch[3] || null;
-        const twitchChannel = isTwitch[4] || null;
-        const twitchCollection = isTwitch[5] || null;
-        const twitchVODOffset = isTwitch[6] || null;
+    const twitchClip = isTwitchClip ? isTwitchClip[1] || isTwitchClip[2] : null;
+    const twitchVOD = isTwitch ? isTwitch[1] : null;
+    const twitchChannel = isTwitch ? isTwitch[2] : null;
+    const twitchCollection = isTwitch ? isTwitch[3] : null;
+    const twitchVODOffset = isTwitch ? isTwitch[4] : null;
 
-        const basePlayerUrl = "https://player.twitch.tv/?";
-        const baseClipUrl = "https://clips.twitch.tv/embed?clip=";
-        const endUrl = "&parent=www.shacknews.com&autoplay=true&muted=false";
+    const basePlayerUrl = "https://player.twitch.tv/?";
+    const baseClipUrl = "https://clips.twitch.tv/embed?clip=";
+    const endUrl = "&parent=www.shacknews.com&autoplay=true&muted=false";
 
-        const channel = twitchChannel ? `channel=${twitchChannel}` : "";
-        const video = twitchVOD ? `video=v${twitchVOD}` : "";
-        const videoOffset = twitchVODOffset ? `&time=${twitchVODOffset}` : "";
-        const collection = twitchCollection ? `collection=${twitchCollection}` : "";
-        const clip = twitchClip || "";
-        const h = { type: "iframe", href };
+    const channel = twitchChannel ? `channel=${twitchChannel}` : "";
+    const video = twitchVOD ? `video=v${twitchVOD}` : "";
+    const videoOffset = twitchVODOffset ? `&time=${twitchVODOffset}` : "";
+    const collection = twitchCollection ? `collection=${twitchCollection}` : "";
+    const clip = twitchClip || "";
+    const h = { type: "iframe", href };
 
-        if (video && !collection)
-            return { ...h, src: `${basePlayerUrl}${video}${endUrl}${videoOffset}` } as ParsedResponse;
-        else if (video && collection)
-            return { ...h, src: `${basePlayerUrl}${video}&${collection}${endUrl}${videoOffset}` } as ParsedResponse;
-        else if (collection) return { ...h, src: `${basePlayerUrl}${collection}${endUrl}` } as ParsedResponse;
-        else if (channel) return { ...h, src: `${basePlayerUrl}${channel}${endUrl}` } as ParsedResponse;
-        else if (clip) return { ...h, src: `${baseClipUrl}${clip}${endUrl}` } as ParsedResponse;
-    }
-    return null;
+    //https://www.twitch.tv/clintstevens/clip/EphemeralUnsightlyCarrotLeeroyJenkins
+    //https://player.twitch.tv/?video=vEphemeralUnsightlyCarrotLeeroyJenkins&parent=www.shacknews.com&autoplay=true&muted=false
+
+    if (video && !collection) return { ...h, src: `${basePlayerUrl}${video}${endUrl}${videoOffset}` } as ParsedResponse;
+    else if (video && collection)
+        return { ...h, src: `${basePlayerUrl}${video}&${collection}${endUrl}${videoOffset}` } as ParsedResponse;
+    else if (collection) return { ...h, src: `${basePlayerUrl}${collection}${endUrl}` } as ParsedResponse;
+    else if (channel) return { ...h, src: `${basePlayerUrl}${channel}${endUrl}` } as ParsedResponse;
+    else if (clip) return { ...h, src: `${baseClipUrl}${clip}${endUrl}` } as ParsedResponse;
+    else return null;
 };
 
 export const isTwitch = (href: string) => parseLink(href);
