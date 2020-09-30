82a0b825336fe81eeba52bb337d3b297208f556f
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-09-30 12:45:26 -0600

    Silence linter in UserFilter FC

--- src/builtin/userpopup/UserFilter.tsx
+++ src/builtin/userpopup/UserFilter.tsx
@@ -1,3 +1,4 @@
+/* eslint react-hooks/exhaustive-deps: 0 */
 import React, { useEffect, useState } from "react";
 import { addFilter, enabledContains, filtersContains, removeFilter } from "../../core/settings";
 
