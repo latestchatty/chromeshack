663e3ffed89be93036b9d9a06b67ddaeff937a1e
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-10-17 11:13:05 -0600

    Fix regressions in PostLengthCounter

--- src/builtin/post_length_counter.ts
+++ src/builtin/post_length_counter.ts
@@ -6,23 +6,23 @@ export const PostLengthCounter = {
     updateTimer: null as ReturnType<typeof setTimeout>,
 
     install() {
-        processPostBoxEvent.addHandler(PostLengthCounter.apply);
-    },
-
-    apply(postbox: HTMLElement) {
-        // script is already injected
-        if (postbox?.querySelector(".post_length_counter_text")) return;
-        // install a div at the top of the post box
-        const position = postbox?.querySelector("div.csubmit");
-        if (!position) return;
-        const child = document.createElement("div");
-        child.setAttribute("class", "post_length_counter_text");
-        position.parentNode.insertBefore(child, position);
-        processPostBoxEvent.addHandler(() => PostLengthCounter.plcHandler(postbox));
+        processPostBoxEvent.addHandler((postbox: HTMLElement) => {
+            const position = postbox?.querySelector("div.csubmit");
+            if (postbox?.querySelector(".post_length_counter_text") || !position) return;
+            const child = document.createElement("div");
+            child.setAttribute("class", "post_length_counter_text");
+            position.parentNode.insertBefore(child, position);
+            PostLengthCounter.update(postbox);
+            const updater = () => PostLengthCounter.update(postbox);
+            postbox.querySelector("textarea#frm_body").addEventListener("keyup", () => {
+                if (PostLengthCounter.updateTimer) clearTimeout(PostLengthCounter.updateTimer);
+                PostLengthCounter.updateTimer = setTimeout(updater, 250);
+            });
+        });
     },
 
     update(postbox: HTMLElement) {
-        const counter = postbox?.querySelector("div.post_length_counter_text") as HTMLElement;
+        const counter = postbox?.querySelector(".post_length_counter_text") as HTMLElement;
         const textarea = postbox?.querySelector("textarea#frm_body") as HTMLInputElement;
         const rawPostText = textarea?.value;
         const encodedText = rawPostText && EmojiPoster.handleEncoding(rawPostText);
@@ -34,14 +34,5 @@ export const PostLengthCounter = {
                 charCount > PostLengthCounter.MAX_POST_BYTES
                     ? "The post preview will be truncated."
                     : `Characters remaining in post preview: ${PostLengthCounter.MAX_POST_BYTES - charCount}`;
-    },
-
-    plcHandler(postbox: HTMLElement) {
-        PostLengthCounter.update(postbox);
-        // only update the counter when they stop typing for performance
-        postbox?.querySelector("textarea#frm_body").addEventListener("keyup", () => {
-            if (PostLengthCounter.updateTimer) clearTimeout(PostLengthCounter.updateTimer);
-            PostLengthCounter.updateTimer = setTimeout(PostLengthCounter.update, 250);
-        });
     },
 };
