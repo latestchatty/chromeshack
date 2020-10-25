d799255f6deee4bf6fcd90dc0688437235aa0957
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-10-24 23:08:12 -0600

    Fixed busted scrollByKeyFix in single-thread mode

--- src/patches/scrollByKeyFix.ts
+++ src/patches/scrollByKeyFix.ts
@@ -6,35 +6,39 @@ export const scrollByKeyFix = async () => {
             code: /*javascript*/ `
 /// monkeypatch nuChatty's broken scroll-post-by-A/Z functionality
 if (!document.getElementById("scrollbykeyfix-wjs")) {
+    function check_event_target(a) {
+        // use chrome/firefox methods to check the event target
+        return a.target && a.target.nodeName !== "TEXTAREA" && a.target.nodeName !== "INPUT";
+    }
     function chat_onkeypress(b) {
-        if (!b) {
-            b = window.event;
-        }
-        let a = String.fromCharCode(b.keyCode);
-        if (sLastClickedItem != -1 && sLastClickedRoot != -1 && check_event_target(b)) {
-            if (a == "Z") {
-                id = get_item_number_from_item_string(get_next_item_for_root(sLastClickedRoot, sLastClickedItem));
-                if (id != false) {
-                    clickItem(sLastClickedRoot, id);
-                    let elem = document.querySelector(\`li#item_\${id} span.oneline_body\`);
-                    elem.click();
-                }
-            }
-            if (a == "A") {
-                id = get_item_number_from_item_string(get_prior_item_for_root(sLastClickedRoot, sLastClickedItem));
-                if (id != false) {
-                    clickItem(sLastClickedRoot, id);
-                    let elem = document.querySelector(\`li#item_\${id} span.oneline_body\`);
-                    elem.click();
-                }
-            }
+        if (!b) b = window.event;
+        const first_load = sLastClickedItem === -1 && sLastClickedRoot === -1;
+        const _url = window.location.href;
+        const _postid = _url && _url.split("item_")[1];
+        const _item = _postid && document.querySelector(\`li#item_\${_postid}\`);
+        const _root = _item && _item.closest("div.root");
+        const _rootid = _root && parseInt(_root.id.substr(5));
+        if (first_load && _rootid && _postid) clickItem(_rootid, _postid);
+
+        const is_valid_target = sLastClickedItem !== -1 && sLastClickedRoot !== -1 && check_event_target(b);
+        const a = String.fromCharCode(b.keyCode);
+        const id = a === "Z"
+            ? get_item_number_from_item_string(get_next_item_for_root(sLastClickedRoot, sLastClickedItem))
+            : a === "A"
+            ? get_item_number_from_item_string(get_prior_item_for_root(sLastClickedRoot, sLastClickedItem))
+            : false;
+
+        if (is_valid_target && id) {
+            clickItem(sLastClickedRoot, id);
+            const elem = document.querySelector(\`li#item_\${id} span.oneline_body\`);
+            elem.click();
         }
         return true;
     }
 
     let scrollByKeyFix = document.createElement("script");
     scrollByKeyFix.id = "scrollbykeyfix-wjs";
-    scrollByKeyFix.textContent = chat_onkeypress.toString();
+    scrollByKeyFix.textContent = \`\${check_event_target.toString()}\${chat_onkeypress.toString()}\`;
     let bodyRef = document.getElementsByTagName("body")[0];
     bodyRef.appendChild(scrollByKeyFix);
     undefined;
