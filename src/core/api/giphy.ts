daa6b4bfdf9caeea744355d72859fa3fcd3a49fb
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-10-24 20:45:34 -0600

    Fix Giphy regex not matching some valid links

--- src/core/api/giphy.ts
+++ src/core/api/giphy.ts
@@ -1,10 +1,10 @@
 import type { ParsedResponse } from "./";
 
 const parseLink = (href: string) => {
-    const isGiphy = /https?:\/\/(?:.+\.)?giphy\.com\/(?:.+\/(\w+)\/|.+-(\w+))/i.exec(href);
+    const isGiphy = /https?:\/\/(?:.+\.)?giphy\.com\/(?:embed\/(\w+)(?:\/.+?)?|.+\/(\w+)\/|.+-(\w+))/i.exec(href);
     return isGiphy
         ? ({
-              src: `https://media.giphy.com/media/${isGiphy[1] || isGiphy[2]}/giphy.mp4`,
+              src: `https://media0.giphy.com/media/${isGiphy[1] || isGiphy[2] || isGiphy[3]}/giphy.mp4`,
               type: "video",
           } as ParsedResponse)
         : null;
