b8adbf2d403302cc1a5052ab3ec41b882007e371
WombatFromHell <WombatFromHell@users.noreply.github.com>
2020-10-31 09:58:50 -0600

    Refactored singleThreadFix

--- src/patches/singleThreadFix.ts
+++ src/patches/singleThreadFix.ts
@@ -1,20 +1,19 @@
 import { scrollToElement } from "../core/common";
+import { fullPostsCompletedEvent } from "../core/events";
 
 /*
  *  Fix visible fullpost position when opening a post in single-thread mode
  */
 export const singleThreadFix = () => {
     const fix = () => {
-        const urlRgx = window.location.href.match(/#item_(\d+)/);
-        const postid = urlRgx && urlRgx[1];
-        const post = document.getElementById(`item_${postid}`);
-        setTimeout(() => {
-            if (post) {
-                console.log("scrolling to single-thread:", post);
-                scrollToElement(post, { toFit: true });
-            }
-        }, 0);
-        window.removeEventListener("load", fix);
+        const urlRgx = window.location.href.match(/id=(\d+)#item_(\d+)/);
+        const rootid = parseInt(urlRgx?.[1]);
+        const postid = parseInt(urlRgx?.[2]);
+        const post = rootid === postid && document.getElementById(`item_${postid}`);
+        if (post) {
+            console.log("scrolling to single-thread:", post);
+            scrollToElement(post, { toFit: true });
+        }
     };
-    window.addEventListener("load", fix);
+    fullPostsCompletedEvent.addHandler(fix);
 };
