61e2da4255728b69935e6f8855ab91b4e6429aba
WombatFromHell <WombatFromHell@users.noreply.github.com>
2023-09-10 19:11:25 -0600

    Add a nix-shell flake as a convenience

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs.buildPackages; [
+    nodejs_20
+    nodePackages.prettier
+  ];
+}
