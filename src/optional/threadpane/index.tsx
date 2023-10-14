a1318dfcf23deebbce866c75e20d303a42037ef6
WombatFromHell <WombatFromHell@users.noreply.github.com>
2023-10-14 08:36:34 -0600

    Clean up guard in threadpane installer

--- src/optional/threadpane/index.tsx
+++ src/optional/threadpane/index.tsx
@@ -8,11 +8,13 @@ import { ThreadPaneApp } from "./ThreadPaneApp";
 const ThreadPane = {
     async install() {
         const enabled = await enabledContains(["thread_pane"]);
+        if (!enabled) return;
+
         const chatty = document.getElementById("newcommentbutton");
         const testing = await getEnabledSuboption("testing_mode");
         const container = document.querySelector("div#cs_thread_pane");
         // only enable thread pane on the main Chatty
-        if ((testing || chatty) && enabled && !container) {
+        if (testing || (chatty && !container)) {
             // apply css to make room for threadpane div
             document.querySelector("body")?.classList?.add("cs_thread_pane_enable");
             const rootEl = document.getElementById("page");
