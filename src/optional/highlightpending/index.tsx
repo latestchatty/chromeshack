ffd56bfb208d2023b79a686b360cbdf297e571d1
WombatFromHell <WombatFromHell@users.noreply.github.com>
2023-10-14 08:37:15 -0600

    Fixed HPP overlay arrows in threadpane mode

--- src/optional/highlightpending/index.tsx
+++ src/optional/highlightpending/index.tsx
@@ -8,12 +8,12 @@ import { HighlightPendingApp } from "./HighlightPendingApp";
 const HighlightPendingPosts = {
     async install() {
         const isEnabled = await enabledContains(["highlight_pending_new_posts"]);
-        const selectedPage = document.querySelector("a.selected_page") as HTMLAnchorElement;
-        const isFirstPage = selectedPage?.href?.match(/page=1$/i);
-        const isChatty = !!(document.getElementById("newcommentbutton") && isFirstPage);
+        if (!isEnabled) return;
+
+        const isChatty = !!document.getElementById("newcommentbutton");
         const container = document.querySelector("#hpnp__app__container");
         const positionElem = document.querySelector(".header-bottom .logo.alt > a");
-        if (isEnabled && !container && positionElem) {
+        if (!container && positionElem) {
             const appContainer = parseToElement(`<div id="hpnp__app__container" />`) as HTMLElement;
             // put our HPNP app next to the Shacknews logo in the top-left
             positionElem.parentElement.classList?.add("hpnp__enabled");
