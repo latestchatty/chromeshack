ffbfd3dc5d73d7630941d1198502513c4f3bc14a
WombatFromHell <WombatFromHell@users.noreply.github.com>
2023-10-17 15:18:46 -0600

    Switched to swc react plugin for vite

--- vite.content.config.ts
+++ vite.content.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
