e2c534b91c723db62b86ae27fb54327a2299494d
WombatFromHell <WombatFromHell@users.noreply.github.com>
2023-10-20 09:06:06 -0600

    Reverted vite react swc plugin dep

--- vite.serviceWorker.config.ts
+++ vite.serviceWorker.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
