00e6a472e59ba45bce629c6ae45ed860434ef9d8
WombatFromHell <WombatFromHell@users.noreply.github.com>
2024-03-12 22:05:32 -0600

    Try to better handle aspect ratio of carousel slides

--- src/core/useResolvedLinks/CarouselButtons.tsx
+++ src/core/useResolvedLinks/CarouselButtons.tsx
@@ -2,14 +2,14 @@ import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ChevronLeft = () => <FontAwesomeIcon className="embla__button__svg right" icon={faChevronLeft} />;
-export const PrevButton = ({ enabled, onClick }: { enabled: boolean; onClick: any }) => (
+export const PrevButton = ({ enabled, onClick }: { enabled: boolean; onClick: () => void }) => (
   <button className="embla__button embla__button--prev" onClick={onClick} disabled={!enabled}>
     <ChevronLeft />
   </button>
 );
 
 const ChevronRight = () => <FontAwesomeIcon className="embla__button__svg right" icon={faChevronRight} />;
-export const NextButton = ({ enabled, onClick }: { enabled: boolean; onClick: any }) => (
+export const NextButton = ({ enabled, onClick }: { enabled: boolean; onClick: () => void }) => (
   <button className="embla__button embla__button--next" onClick={onClick} disabled={!enabled}>
     <ChevronRight />
   </button>
